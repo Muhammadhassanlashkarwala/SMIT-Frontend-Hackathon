4ef24baa75f44fa062b8eaa04bc9f93a8c7958ce
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-01-26 09:00:05 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
 import Login from "./Auth/Login";
 import Signup from "./Auth/Signup";
-import { Home } from "./Pages/Home";
-import { AdminHeading } from "./Admin/AdminHeading";
-import Dashboards from "./Admin/AdminComponents/Dashboard";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import { CalculatorLoan } from "./components/CalculatorLoan";
-// import RequestManager from "./Admin/AdminComponents/RequestManager";
+import User from "./user/user";
+import WeddingLoans from "./user/userComponents/WeddingLoans";
+import ConstructionLoans from "./user/userComponents/ConstructionLoans";
+import BussinessLoans from "./user/userComponents/BussinessLoans";
+import EducationalLoans from "./user/userComponents/EducationalLoans";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<User />}></Route>
+          <Route path="/home" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/admin" element={<AdminHeading />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/admin" element={<AdminHeading />} />
-        <Route path="/admindashboards" element={<Dashboards />} />
-        {/* <Route
-          path="/admindashboard/requestmanager"
-          element={<RequestManager />} /> */}
+          <Route path="/weddingloans" element={<WeddingLoans />}></Route>
+          <Route
+            path="/constructionloans"
+            element={<ConstructionLoans />}
+          ></Route>
+          <Route path="/bussinessloans" element={<BussinessLoans />}></Route>
+          <Route path="/educationloans" element={<EducationalLoans />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
     </>
